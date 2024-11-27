18ed6963698d36247a0350aaf9a3942f480cb72e
krschan <krischxn@gmail.com>
2024-11-27 18:16:25 +0100

    FEAT(CategoryController): Add Crud methods.

---
+++ backend/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller {
+    // Get all resources (index)
+    public function index() {
+        // Empty method
+    }
+
+    // Get a specific resource by ID (show)
+    public function show($id) {
+        // Empty method
+    }
+
+    // Create a new resource (store)
+    public function store(Request $request) {
+        // Empty method
+    }
+
+    // Update a specific resource (update)
+    public function update(Request $request, $id) {
+        // Empty method
+    }
+
+    // Delete a specific resource (destroy)
+    public function destroy($id) {
+        // Empty method
+    }
+}
