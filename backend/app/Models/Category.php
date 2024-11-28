bbd507ffb3738b4627cfe0ccbae86460edec3129
Dynavy <pl2023379@365.stucom.com>
2024-11-28 13:33:50 +0100

    Created Category model with mass assignment and restaurant relation
    - Created the `Category` model in the `app/Models` directory using the artisan command.
    
    - Added `$fillable` property for mass assignment protection on the `name` attribute.
    
    - Set up a `belongsTo` relationship with the `Restaurant` model, preparing for future database interaction.

---
+++ backend/app/Models/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    // This trait allows the model to use Laravel factories for creating new instances.
+    // Factories help create realistic sample data for development, testing, and seeding purposes.
+    use HasFactory;
+
+    // Allows to mass assign data to the name attribute when creating or updating records.
+    protected $fillable = ['name'];
+
+    /**
+     * Define the N:M relationship between Category and Restaurant.
+     * 
+     * This method establishes that a category can be associated with multiple
+     * restaurants through the table 'restaurant_categories'. It allows 
+     * us to retrieve the restaurants related to a specific category using 
+     * Eloquent's relationship features.
+     */
+    public function restaurants()
+    {
+        return $this->belongsToMany(Restaurant::class, 'restaurant_categories');
+    }
+}
