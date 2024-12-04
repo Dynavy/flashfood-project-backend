cd7e32dc3b2915dc98c1517d3891c495bfff94d7
Dynavy <pl2023379@365.stucom.com>
2024-12-04 14:35:56 +0100

    FEAT(RestaurantRequest): validate error messages.

---
+++ backend/app/Http/Requests/RestaurantRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RestaurantRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'address' => 'required|string|max:255',
+            'latitude' => 'required|numeric|between:-90,90',
+            'longitude' => 'required|numeric|between:-180,180',
+            'google_place_id' => 'required|string|unique:restaurants,google_place_id|max:255',
+            'phone' => 'required|string|unique:restaurants,phone|max:15',
+            'website' => ['nullable', 'regex:/^https:\/\/.+$/', 'max:255'],
+            'rating' => 'nullable|numeric|min:0|max:5',
+        ];
+    }
+}
+
