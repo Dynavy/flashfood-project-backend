3a755e2a89465a573567a4f1ab538f16edc29c95
Dynavy <pl2023379@365.stucom.com>
2024-12-04 14:37:06 +0100

    Chore(RestaurantController): moved the logic of status validation.
    - Now the validation about the status is managed on the Handler class.

--- backend/app/Http/Controllers/RestaurantController.php
+++ backend/app/Http/Controllers/RestaurantController.php
@@ -4,47 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\RestaurantService;
-use App\Models\Restaurant;
-
+use App\Http\Requests\RestaurantRequest;
 
 class RestaurantController extends Controller
 {
+    // Instance of RestaurantService.
     protected $restaurantService;
 
-    // Inject CategoryService.
+    // Inject RestaurantService.
     public function __construct(RestaurantService $restaurantService)
     {
         $this->restaurantService = $restaurantService;
     }
 
-    // Create a specific category (create).
-    public function store(Request $request)
+    public function show(RestaurantRequest $request, $id)
     {
-        try {
-            // Delegate the creation logic to the service layer.
-            $restaurant = $this->restaurantService->store($request->all());
-
-            return response()->json([
-                'message' => 'Restaurant created successfully!',
-                'category' => $restaurant,
-            ], 201);
-
-            // Status 422  --> server can't process the request, although it understands it.
-        } catch (\Illuminate\Validation\ValidationException $e) {
-            // Return a JSON response indicating validation failure
-            return response()->json([
-                'message' => 'Validation failed.',
-                'errors' => $e->errors(),
-            ], 422);
-
-        } catch (\Exception $e) {
-            // Handle any other exceptions
-            return response()->json([
-                'message' => 'An error occurred while creating the restaurant.',
-                'error' => $e->getMessage(),
-            ], 500);
-        }
+        $restaurant = $this->restaurantService->showByID($id);
+        return response()->json($restaurant);
     }
+
+    public function findByName(RestaurantRequest $request, $name)
+    {
+        $restaurantName = $this->restaurantService->findByName($name);
+        return response()->json($restaurantName);
+    }
+
+    public function index()
+    {
+        // Pagination in case the restaurnt list is very large.
+        $restaurants = $this->restaurantService->index()->paginate(50);
+        return response()->json($restaurants);
+    }
+
+    // Create a specific category (create).
+    public function store(RestaurantRequest $request)
+    {
+        // Delegate the creation logic to the service layer.
+        $restaurant = $this->restaurantService->store($request->all());
+
+        return response()->json([
+            'message' => 'Restaurant created successfully!',
+            'restaurant' => $restaurant,
+        ], 201);
+    }
+
 
     // Update a specific category (update).
     public function update(Request $request, $id)
@@ -55,24 +58,11 @@ class RestaurantController extends Controller
     // Delete a specific resource (destroy).
     public function destroy($id)
     {
-        try {
-            // Delegate the deletion logic to the service layer.
-            $restaurantData = $this->restaurantService->destroy($id);
+        // Delegate the deletion logic to the service layer.
+        $restaurantName = $this->restaurantService->destroy($id);
 
-            return response()->json([
-                'message' => "Restaurant '{$restaurantData['name']}' located at '{$restaurantData['address']}' has been deleted successfully."
-            ], 200);
-
-        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-            return response()->json([
-                'message' => "Restaurant with id $id not found."
-            ], 404);
-
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'An error occurred while deleting the Restaurant.',
-                'error' => $e->getMessage()
-            ], 500);
-        }
+        return response()->json([
+            'message' => "Category $restaurantName with id $id deleted successfully."
+        ], 200);
     }
 }
