d90d641067521855bb7961a6829d447605673116
Dynavy <pl2023379@365.stucom.com>
2024-12-04 14:38:03 +0100

    CHORE(CategoryFactory): Deleted unnecessary comments.

--- backend/database/factories/CategoryFactory.php
+++ backend/database/factories/CategoryFactory.php
@@ -5,17 +5,8 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Category;
 
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
- */
 class CategoryFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     protected $model = Category::class;
 
     // Template food categories.
