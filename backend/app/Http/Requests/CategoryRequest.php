00fef34e90d5ba4add30699da4baa041ec7151a6
Dynavy <pl2023379@365.stucom.com>
2024-12-05 15:17:11 +0100

    FEAT(CategoryRequest): implement 422 validation for Categories.

---
+++ backend/app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class CategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:50|unique:categories,name',
+        ];
+    }
+
+    // Status 422  --> Server can't process the request, although it understands it.
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => $validator->errors(),
+            ], 422)
+        );
+    }
+}
