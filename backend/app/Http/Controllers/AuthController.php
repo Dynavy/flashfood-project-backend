bed9132d54becfb40d9d6a67965f1ec3fbcb7408
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2024-12-27 19:44:22 +0100

    FEAT(AuthController): implemented login method.
    - Validates de data throught the AuthRequest loginRules() method.
    
    - Uses the business logic of AuthService.

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -15,10 +15,23 @@ class AuthController extends Controller
 
     public function register(AuthRequest $request): JsonResponse
     {
-        $user = $this->authService->register($request->validated());
+        $validatedData = $request->validate($request->registerRules());
 
+        $user = $this->authService->register($validatedData);
         return response()->json([
             'message' => 'User registered successfully.',
+            'user' => $user,
+        ], 201);
+    }
+
+    public function login(AuthRequest $request): JsonResponse
+    {
+        $validatedData = $request->validate($request->loginRules());
+
+        $user = $this->authService->login($validatedData);
+
+        return response()->json([
+            'message' => 'User logged successfully.',
             'user' => $user,
         ], 201);
     }
