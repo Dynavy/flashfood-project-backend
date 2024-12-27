8cc236651bc26cae0e8f131a3733f37410c00215
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2024-12-27 19:42:18 +0100

    FEAT(AuthService): implemented login method logic.
    - Check if the user is authenticated.
    
    - Creates a token if the user is valid.

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -3,7 +3,7 @@
 namespace App\Services;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthService
 {
@@ -16,5 +16,26 @@ class AuthService
         ]);
 
         return $user;
+    }
+
+
+    public function login(array $credentials): array
+    {
+
+        // Throws authentication exception if the user its invalid.
+        if (!Auth::attempt($credentials)) {
+            throw new \Exception('Invalid credentials.', 401);
+        }
+
+        // Get the user data if authenticated.
+        $user = Auth::user();
+
+        // Generates personal token for the user trought Sanctum.
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
     }
 }
