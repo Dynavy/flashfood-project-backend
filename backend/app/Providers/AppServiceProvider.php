a28bb805a02b7a1a000c4b9e385c3464a603edbe
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2025-01-08 14:49:07 +0100

    FEAT(AppServiceProvider): registered the Blade component.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use App\Exceptions\CustomExceptionHandler;
 
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register the Blade component.
+        Blade::component('mail-header', \App\View\Components\MailHeader::class);
     }
 }
