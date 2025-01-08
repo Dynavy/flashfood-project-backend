d0ece7d6d5f41bf7bbe3179e43a8df947c0a74a2
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2025-01-08 14:47:02 +0100

    FEAT(ResetPasswordEmail): add password reset email view.
    - Added Blade view for the password reset form (`reset-password-email.blade.php`).
    
    - Styled the email content with CSS (`resetLinkEmail.css`).

---
+++ backend/resources/views/emails/reset-password-email.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlashFoods Password Reset</title>
+    <link href="{{ asset('css/reset-password-email.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Header component: resources/views/components/mail/mail-header.blade.php -->
+    <x-mail.mail-header />
+
+    <div class="content">
+        <h1 class="title">Restablecer contraseña</h1>
+        <p class="message">Hemos recibido una solicitud para restablecer tu contraseña. Haz clic en el siguiente enlace para crear una nueva contraseña:</p>
+        <p>
+            <a href="{{ $actionUrl }}" class="button">
+                Click para restablecer tu contraseña
+            </a>
+        </p>
+        <p class="support-text">
+            Si no puedes hacer clic en el botón, copia y pega el siguiente enlace en un navegador:<br>
+            <span class="reset-link">{{ $actionUrl }}</span>
+        </p>
+        <p class="warning-text">
+            Si no fuiste tú quien solicitó este cambio de contraseña, por favor, ignora este correo.
+        </p>
+    </div>
+    <div class="footer">
+        © {{ date('Y') }} {{ config('app.name') }}. Todos los derechos reservados.
+    </div>
+    </div>
+</body>
+
+</html>
