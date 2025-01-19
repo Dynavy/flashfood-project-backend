bd83988fc3485734c38e7bb615c93acb24035a27
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2025-01-19 19:41:56 +0100

    REFACTOR(UserTableSeeder): use fixedUser() method for creating admin user

--- backend/database/seeders/UserTableSeeder.php
+++ backend/database/seeders/UserTableSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserTableSeeder extends Seeder
 {
@@ -13,11 +13,14 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::factory()->fixedUser([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
-            'password' => Hash::make('password123'),
-        ]);
+            'password' => 'password123',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ])->create();
+        ;
 
         User::factory()->count(10)->create();
     }
