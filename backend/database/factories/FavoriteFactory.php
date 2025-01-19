50b448f7b02dcd434bc3ac17af5dfd7be7565425
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2025-01-19 19:38:34 +0100

    FIX(FavoriteFactory):  use existing records in favorite factory.

--- backend/database/factories/FavoriteFactory.php
+++ backend/database/factories/FavoriteFactory.php
@@ -2,19 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\Favorite;
 use App\Models\User;
 use App\Models\Restaurant;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class FavoriteFactory extends Factory
 {
-    protected $model = \App\Models\Favorite::class;
+    protected $model = Favorite::class;
 
     public function definition()
     {
+        // Ensures user_id and restaurant_id use existing records or create new ones if none exist.
         return [
-            'user_id' => User::factory(),
-            'restaurant_id' => Restaurant::factory(),
+            'user_id' => User::inRandomOrder()->value('id') ?? User::factory(),
+            'restaurant_id' => Restaurant::inRandomOrder()->value('id') ?? Restaurant::factory(),
         ];
     }
 }
